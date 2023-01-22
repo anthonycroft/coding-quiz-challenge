a16257a05a73565051a1d28b1deb494f621d0228
Anthony Croft <ajcsignups@gmail.com>
2023-01-22 13:00:33 +0000

    Added further questions to the questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -11,12 +11,60 @@ const quiz = [
       answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
       correct: 3
     },
-    { question: 'String values must be stored within  ______ when being assigned to variables.',
+    { question: 'String values must be stored in  ______ when being assigned to variables.',
       answers: ['commas', 'curly brackets', 'quotes', 'parentheses'],
       correct: 2
     },
     { question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
       answers: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
       correct: 3
+    },
+    { question: 'Which of the following keywords is used to define a variable in javascript:',
+      answers: ['var', 'let', 'both 1 and 2', 'none of the above'],
+      correct: 2
+    },
+    { question: 'Which of the following is not a javascript framework:',
+      answers: ['Vue', 'React', 'Node', 'Laravel'],
+      correct: 3
+    },
+    { question: 'Inside which HTML element do we write JavaScript:',
+      answers: ['<script>', '<js>', '<javascript>', '<java>'],
+      correct: 0
+    },
+    { question: 'Which one of the following is not a comparison operator:',
+      answers: ['=     ', '<     ', '>     ', '!=    '],
+      correct: 0
+    },
+    { question: 'DOM stands for ______ in relation to the browser.',
+      answers: ['Document Object Model', 'Document Object Manipulation', 'Document Objective Model', 'none of above'],
+      correct: 0
+    },
+    { question: 'Everything in JavaScript is either a ______',
+      answers: ['primitive or object', 'function or object', '...everything is an object', 'number or object'],
+      correct: 0
+    },
+    { question: 'An external JavaScript file must contain the <script> tag?',
+      answers: ['False', 'True'],
+      correct: 0
+    },
+    { question: 'How do you write "Hello World" in an alert box?',
+      answers: ['msgBox("Hello World");', 'msg("Hello World");', 'alert("Hello World");', 'alertBox("Hello World");'],
+      correct: 2
+    },
+    { question: 'How do create a function in javascript?',
+      answers: ['function myFunction()', 'function:myFunction()', 'function = myFunction()'],
+      correct: 0
+    },
+    { question: 'How do call a function named "myFunction"?',
+      answers: ['call myFunction()', 'myFunction()', 'call myFunction myFunction()'],
+      correct: 1
+    },
+    { question: 'How do you write an IF statement in JavaScript?',
+      answers: ['if i = 5 then', 'if i = 5', 'if i == 5 then', 'if (i == 5)'],
+      correct: 3
+    },
+    { question: 'How does a WHILE loop start?',
+      answers: ['while (i <= 10)', 'while i = 1 to 10', 'while (i <= 10; i++)'],
+      correct: 0
     }
   ]
